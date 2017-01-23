3ca2f03c90dab5d180a3d8aa48ae367045cda18f
Soumya Kedlaya <soumyakedlaya@Soumyas-MacBook-Air.local>
2017-01-23 12:56:13 -0800

    updated main.js

--- apps/project/static/project/js/main.js
+++ apps/project/static/project/js/main.js
@@ -3,7 +3,9 @@ $(document).ready(function(){ // executes when HTML-Document is loaded and DOM i
 
   $(window).load(function() {  // executes when complete page is fully loaded, including all frames, objects and images
 
-    //DEFINE ALL GLOBAL VARIABLES
+    //GLOBAL VARIABLES
+    var gLat = 37.7749 //SF lat
+    var gLong = -122.4194 //SF long
     var gAssaultStr = "ASSAULT"
     var gAssaultPng = "/static/project/images/crime/ASSAULT.png"
     var gSexOffensesStr = "SEX OFFENSES, FORCIBLE"
@@ -18,21 +20,26 @@ $(document).ready(function(){ // executes when HTML-Document is loaded and DOM i
     var gLimitCount = "&$limit=100"
     var gUrl;
     var gCrimeMarkers = [];
-    var gResidenceUrlString = "";
-    gResidenceImagePng = "";
+    var gResidenceUrlString = ""
+    var gResidenceImagePng = ""
+    var gResidenceImageStr = ""
+    var gResidenceMatchStr = ""
+    var gHomeStr = "house.png"
     var gHomePng = "/static/project/images/housing/house.png"
+    var gCondoStr = "condo.png"
     var gCondoPng = "/static/project/images/housing/condo.png"
+    var gRentStr = "rent.png"
     var gRentPng = "/static/project/images/housing/rent.png"
     var gHousingMarkers = [];
     var gPrevInfoWindow;
 
     //set up url for external data.gov.
-    function setUp_gUrl(catToFilterData) {
-      gUrl = gUrlPathPrefix + encodeURIComponent(catToFilterData) + gLimitCount;
+    function setUp_gUrl() {
+      gUrl = gUrlPathPrefix + encodeURIComponent(gCrimeString) + gLimitCount;
     }
 
     //########################## INITIALIZE MAP ########################
-      var center = new google.maps.LatLng(37.7749,-122.4194); //SF coordinates
+      var center = new google.maps.LatLng(gLat, gLong);
       var mapOptions = {
         zoom: 12,
         center: center,
@@ -95,7 +102,7 @@ $(document).ready(function(){ // executes when HTML-Document is loaded and DOM i
 
       //###################DISPLAY CRIME MARKERS ON MAP ##############
       function displayCrimeMarker() {
-        setUp_gUrl(gCrimeString);
+        setUp_gUrl();
 
         $.getJSON(gUrl, function(data, textstatus) {
           // console.log("In getJSON")
@@ -113,7 +120,9 @@ $(document).ready(function(){ // executes when HTML-Document is loaded and DOM i
                       visible: true,
                       content: value
                   });
-                  gCrimeMarkers.push(marker); //push marker into a global array of all the CrimeMarkers
+
+                  //push marker into a global array of all the CrimeMarkers
+                  gCrimeMarkers.push(marker);
 
                   //INFO WINDOW FOR EACH MARKER
                   marker.addListener('click', function(){
@@ -138,94 +147,72 @@ $(document).ready(function(){ // executes when HTML-Document is loaded and DOM i
         });//end .getJSON
       }//end displayCrimeMarker
 
+      //When box unchecked, set visibility to false and close info window
+      function crimeBoxUnChecked (){
+        $.each(gCrimeMarkers, function(index, value){
+          if(value.icon.includes(gCrimeString)){
+            value.setVisible(false);
+          }
+          if(gPrevInfoWindow){
+            if(gPrevInfoWindow.content.includes(gCrimeString)){
+              gPrevInfoWindow.close();
+            }
+          }
+        }); //end .each
+      }
+
     //##################### CLICK CHECKBOX TO SELECT VARIOUS CRIME CATEGORIES #################
 
-      $('#assault').click(function(){
-         if($('#assault').is(':checked')){
-           gCrimeString = gAssaultStr;
-           gCrimeImagePng = gAssaultPng;
-           displayCrimeMarker();
-         }
-         else{
-           $.each(gCrimeMarkers, function(index, value){
-             if(value.icon.includes("ASSAULT")){
-               value.setVisible(false);
-             }
-             if(gPrevInfoWindow){
-               if(gPrevInfoWindow.content.includes("ASSAULT")){
-                 gPrevInfoWindow.close();
-               }
-             }
-         }); //end .each
-       } //end else
-     });
+    $('#assault').click(function(){
+      gCrimeString = gAssaultStr;
+      if($('#assault').is(':checked')){
+        gCrimeImagePng = gAssaultPng;
+        displayCrimeMarker();
+      }
+      else{
+        crimeBoxUnChecked();
+      }
+    });
 
-     $('#sexoffense').click(function(){
-        if($('#sexoffense').is(':checked')){
-          gCrimeString = gSexOffensesStr;
-          gCrimeImagePng = gSexOffensesPng;
-          displayCrimeMarker();
-        }
-        else{
-          $.each(gCrimeMarkers, function(index, value){
-            if(value.icon.includes("SEX OFFENSES, FORCIBLE.png")){
-              value.setVisible(false);
-            }
-            if(gPrevInfoWindow){
-              if(gPrevInfoWindow.content.includes("SEX OFFENSES, FORCIBLE")){
-                gPrevInfoWindow.close();
-              }
-            }
-        }); //end .each
-      } //end else
+    $('#sexoffense').click(function(){
+      gCrimeString = gSexOffensesStr;
+      if($('#sexoffense').is(':checked')){
+        gCrimeImagePng = gSexOffensesPng;
+        displayCrimeMarker();
+      }
+      else{
+        crimeBoxUnChecked();
+      }
     });
 
     $('#kidnapping').click(function(){
-       if($('#kidnapping').is(':checked')){
-         gCrimeString = gKidnappingStr;
-         gCrimeImagePng = gKidnappingPng;
-         displayCrimeMarker();
-       }
-       else{
-         $.each(gCrimeMarkers, function(index, value){
-           if(value.icon.includes("KIDNAPPING.png")){
-             value.setVisible(false);
-           }
-           if(gPrevInfoWindow){
-             if(gPrevInfoWindow.content.includes("KIDNAPPING")){
-               gPrevInfoWindow.close();
-             }
-           }
-         }); //end .each
-       } //end else
-   });
+      gCrimeString = gKidnappingStr;
+      if($('#kidnapping').is(':checked')){
+        gCrimeImagePng = gKidnappingPng;
+        displayCrimeMarker();
+      }
+      else{
+        crimeBoxUnChecked();
+      }
+    });
 
-   $('#arson').click(function(){
+    $('#arson').click(function(){
+      gCrimeString = gArsonStr;
       if($('#arson').is(':checked')){
-        gCrimeString = gArsonStr;
         gCrimeImagePng = gArsonPng;
         displayCrimeMarker();
       }
       else{
-        $.each(gCrimeMarkers, function(index, value){
-          if(value.icon.includes("ARSON.png")){
-            value.setVisible(false);
-          }
-          if(gPrevInfoWindow){
-            if(gPrevInfoWindow.content.includes("ARSON")){
-              gPrevInfoWindow.close();
-            }
-          }
-      }); //end .each
-    } //end else
-  });
+        crimeBoxUnChecked();
+      }
+    });
 
     //################################ HOUSING DATA #################################
-    //used an online converter (http://codebeautify.org/csv-to-xml-json) to convert .csv files to .json. Saved as static data in app.
+    //Online converter (http://codebeautify.org/csv-to-xml-json) to convert .csv files to .json.
+    //Saved as static data in app.
 
     //Display Residence markers on map
     function displayResidenceMarker(residenceCategory) {
-
       $.get(gResidenceUrlString, function(res) {
             //initialize Info Window
             var infowindow = new google.maps.InfoWindow({
@@ -270,87 +257,76 @@ $(document).ready(function(){ // executes when HTML-Document is loaded and DOM i
       });//end $get
     }//end displayResidenceMarker
 
-  //##################### CLICK CHECKBOX TO SELECT VARIOUS HOUSING CATEGORIES #################
-
-    $('#home').click(function(){
-       if($('#home').is(':checked')){
-         gResidenceUrlString = "home";
-         gResidenceImagePng = gHomePng;
-         displayResidenceMarker("Single Family Home");
-       }
-       else{
-         $.each(gHousingMarkers, function(index, value){
-           if(value.icon.includes("house.png")){
-             value.setVisible(false);
-           }
-           if(gPrevInfoWindow){
-             if(gPrevInfoWindow.content.includes("Home")){
-               gPrevInfoWindow.close();
-             }
-           }
-         }); //end each
-      }
-    });//END home.click
-
-     $('#condo').click(function(){
-        if($('#condo').is(':checked')){
-          gResidenceUrlString = "condo";
-          gResidenceImagePng = gCondoPng;
-          displayResidenceMarker("Condo");
+    function housingBoxUnChecked (){
+      $.each(gHousingMarkers, function(index, value){
+        if(value.icon.includes(gResidenceImageStr)){
+          value.setVisible(false);
         }
-        else{
-          $.each(gHousingMarkers, function(index, value){
-            if(value.icon.includes("condo.png")){
-              value.setVisible(false);
-            }
-            if(gPrevInfoWindow){
-              if(gPrevInfoWindow.content.includes("Condo")){
-                gPrevInfoWindow.close();
-              }
-            }
-        });
+        if(gPrevInfoWindow){
+          if(gPrevInfoWindow.content.includes(gResidenceMatchStr)){
+            gPrevInfoWindow.close();
+          }
+        }
+      }); //end each
+    }
+
+  //##################### CLICK CHECKBOX TO SELECT VARIOUS HOUSING CATEGORIES #################
+    $('#home').click(function(){
+      if($('#home').is(':checked')){
+        gResidenceUrlString = "home";
+        gResidenceImagePng = gHomePng;
+        displayResidenceMarker("Single Family Home");
+      }
+      else{
+        gResidenceImageStr = gHomeStr;
+        gResidenceMatchStr = "Home";
+        housingBoxUnChecked();
       }
     });
 
-      $('#rent').click(function(){
-         if($('#rent').is(':checked')){
-           gResidenceUrlString = "rent";
-           gResidenceImagePng = gRentPng;
-           displayResidenceMarker("Rental");
-         }
-         else{
-           $.each(gHousingMarkers, function(index, value){
-             if(value.icon.includes("rent.png")){
-               value.setVisible(false);
-             }
-             if(gPrevInfoWindow){
-               if(gPrevInfoWindow.content.includes("Rent")){
-                 gPrevInfoWindow.close();
-               }
-             }
-         });
-       }
-     });
+    $('#condo').click(function(){
+      if($('#condo').is(':checked')){
+        gResidenceUrlString = "condo";
+        gResidenceImagePng = gCondoPng;
+        displayResidenceMarker("Condo");
+      }
+      else{
+        gResidenceImageStr = gCondoStr;
+        gResidenceMatchStr = "Condo";
+        housingBoxUnChecked();
+      }
+    });
+
+    $('#rent').click(function(){
+      if($('#rent').is(':checked')){
+        gResidenceUrlString = "rent";
+        gResidenceImagePng = gRentPng;
+        displayResidenceMarker("Rental");
+      }
+      else{
+        gResidenceImageStr = gRentStr;
+        gResidenceMatchStr = "Rent";
+        housingBoxUnChecked();
+      }
+    });
 
      //PRICE RANGE FILTER
      $('#filterprice').click(function(){
+       var minPrice = $('#minprice').val();
+       var intMinPrice = parseInt(minPrice); //parseInt converts string to integer
 
-       var minprice = $('#minprice').val();
-       minprice = parseInt(minprice); //parseInt converts string to integer
-
-       var maxprice = $('#maxprice').val();
-       maxprice = parseInt(maxprice);
+       var maxPrice = $('#maxprice').val();
+       var intMaxPrice = parseInt(maxPrice);
 
        $.each(gHousingMarkers, function(index, value){
          var price = value.content.Price;
-         price = price.replace(/,/g, "");
-        //  price = price.replace('/', ""); //replaces all / in number from data  with empty string
-         price = price.replace('$', ""); //replaces $ in number from data with empty string
-         price = price.replace('/mo', ""); //replaces /mo in number from data with empty string
-         price = parseInt(price); //converts string to integer
+         price = price.replace(/,/g, ""); //replaces all '/' in number from data with empty string
+         price = price.replace('$', ""); //replaces '$' in number from data with empty string
+         price = price.replace('/mo', ""); //replaces '/mo' in number from data with empty string
+         var intPrice = parseInt(price); //converts string to integer
 
          //if the price is out of the price range user inputs, set visibility to false
-         if(price < minprice || price > maxprice ){
+         if(intPrice < intMinPrice || intPrice > intMaxPrice ){
            value.setVisible(false);
          }
        });
